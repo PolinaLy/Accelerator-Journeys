444a6dc7f36361734e8b088fdb2d9e916cb570b7
Polina Lysikova <polinaly13@mail.ru>
2023-08-18 15:08:58 +0700

    Проект Путешествия

--- source/js/modules/open-menu.js
+++ source/js/modules/open-menu.js
@@ -3,6 +3,26 @@ const menuButton = document.querySelector('.menu__toggle');
 const body = document.querySelector('body');
 const linkHeader = document.querySelectorAll('.menu a');
 
+function trapFocus(element) {
+  const focusableEls = document.querySelectorAll('header a[href]:not([disabled])');
+  const firstFocusableEl = focusableEls[0];
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function (e) {
+    const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (document.activeElement === lastFocusableEl) {
+      firstFocusableEl.focus();
+      e.preventDefault();
+    }
+  });
+}
+
 const outsideHeaderClick = (evt) => {
   if (evt.target === header && evt.target.closest !== 'header__wrapper') {
     header.classList.remove('open-menu');
@@ -20,7 +40,17 @@ if (document.querySelector('.menu') && menuButton) {
     if (header.classList.contains('open-menu')) {
       body.style.position = 'fixed';
 
+      trapFocus(header);
+
       header.addEventListener('click', outsideHeaderClick);
+
+      header.addEventListener('keydown', function (e) {
+        e.preventDefault();
+        if (e.key === 'Escape' || e.keyCode === 27) {
+          header.classList.remove('open-menu');
+          body.style.position = 'inherit';
+        }
+      });
 
       linkHeader.forEach((el) => {
         el.addEventListener('click', () => {
